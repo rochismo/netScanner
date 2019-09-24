dc12e0e057163d2b1625c11e477e12f54a6f5023
rochismo <alexis.platino@gmail.com>
2019-09-24 21:56:07 +0200

    Fixed things

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
-const Pinger = require("./lib/Pinger");
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const pinger = new Pinger();
-const net = require("network");
-const os = require("os");
-app.set("view engine", "ejs");
-app.set("views", "./views");
+const Server = require("./lib/server")
+const app = require('electron').app;
+const Window = require('electron').BrowserWindow; // jshint ignore:line
+const Tray = require('electron').Tray; // jshint ignore:line
+const Menu = require('electron').Menu; // jshint ignore:line
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const server = new Server();
+let mainWindow = null;
 
-function determineCidr(ip) {
-  const ifaces = os.networkInterfaces();
-  let cidrValue = "192.168.1.0/24"
-  for (const iface in ifaces) {
-    const ifaceObj = ifaces[iface];
-    ifaceObj.forEach(({address, cidr}) => {
-      if (address == ip) {
-        cidrValue = cidr;
-      }
-    })
-  }
-  return cidrValue
+function createWindow() {
+	server.init();
+	server.addRoutes();
+	server.listen();
+	mainWindow = new Window({
+		width: 640,
+		height: 480,
+		autoHideMenuBar: false,
+		useContentSize: true,
+		resizable: true
+		//  'node-integration': false // otherwise various client-side things may break
+	});
+	mainWindow.loadURL('http://localhost:3000/');
 }
 
-determineCidr();
-
-app.get("/", (req, res) => {
-  net.get_active_interface(function(err, {ip_address}) {
-    cidr = determineCidr(ip_address);
-    res.render("index", {ip_cidr: cidr})
-  });
-});
-
-app.get("/pingSweep", async function(req, res) {
-  const ip = req.query.ip;
-  pinger.setRange(ip);
-  pinger.pingSweep();
-  await pinger.fulfillPromisesAndFilterHosts();
-  res.json(pinger.aliveHosts);
-});
-
-app.listen(3000, function() {
-  console.log("http://localhost:3000");
-});
+app.on('ready', createWindow)
